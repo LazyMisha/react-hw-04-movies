b1b0c5a91d5e2b1e1df4857706cbd1c0cbacf32b
Mykhailo Trunov <mykhailo.trunov@synapse.com>
2020-09-23 20:29:47 +0300

    Updates

---
+++ precache-manifest.88c7c21b4409cfa883cef4a052f180dc.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4d23fd876c9b4659e03a48cb5737a7f",
+    "url": "/react-hw-04-movies/index.html"
+  },
+  {
+    "revision": "d08df3d4675a6adf188f",
+    "url": "/react-hw-04-movies/static/css/5.cce6228a.chunk.css"
+  },
+  {
+    "revision": "72f75f3afc33b717d14c",
+    "url": "/react-hw-04-movies/static/css/6.6b0c9779.chunk.css"
+  },
+  {
+    "revision": "3b7da8f2d5b7eedb5e22",
+    "url": "/react-hw-04-movies/static/css/main.faa61506.chunk.css"
+  },
+  {
+    "revision": "c57488a9d846016f38b7",
+    "url": "/react-hw-04-movies/static/js/0.cdc48b03.chunk.js"
+  },
+  {
+    "revision": "10608c09cb64f5a3619f",
+    "url": "/react-hw-04-movies/static/js/3.10306afb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-hw-04-movies/static/js/3.10306afb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c817648b029c5736a7b2",
+    "url": "/react-hw-04-movies/static/js/4.61d93db8.chunk.js"
+  },
+  {
+    "revision": "d08df3d4675a6adf188f",
+    "url": "/react-hw-04-movies/static/js/5.6dc3dfeb.chunk.js"
+  },
+  {
+    "revision": "72f75f3afc33b717d14c",
+    "url": "/react-hw-04-movies/static/js/6.628d8283.chunk.js"
+  },
+  {
+    "revision": "2421dbe7c0151eb69e81",
+    "url": "/react-hw-04-movies/static/js/7.cc1b1ae2.chunk.js"
+  },
+  {
+    "revision": "d8a06bde7ecc44dd28ee",
+    "url": "/react-hw-04-movies/static/js/8.cdaf663e.chunk.js"
+  },
+  {
+    "revision": "3b7da8f2d5b7eedb5e22",
+    "url": "/react-hw-04-movies/static/js/main.638a0a9b.chunk.js"
+  },
+  {
+    "revision": "f7e571d6cd3685402723",
+    "url": "/react-hw-04-movies/static/js/runtime-main.4a1f9069.js"
+  }
+]);
